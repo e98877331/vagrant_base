ca3465ec028bcf31f13a9d31900023d10f3a79b7
chenming.chung <cm.chung@hopebaytech.com>
2014-11-07 00:08:37 +0800

    Fix path error during deploy

--- deploy/test.sh
+++ deploy/test.sh
@@ -5,6 +5,10 @@ apt-get install --no-install-recommends -y python-pip apache2 libapache2-mod-wsg
 pip install django django-appconf django-pipeline django-user-accounts
 
 # TODO install pinax-theme
+if [ -f "/vagrant/deploy/packagies/pinax-theme/setup.py" ];then
+    echo "pinax-theme-bootstrap haven't installed, please read README before install."
+    exit 1
+fi
 cd /vagrant/deploy/packages/pinax-theme/
 python setup.py install
 
@@ -14,10 +18,10 @@ cp -rf /vagrant/base/* /opt/base/www/
 mkdir -p /opt/base/db
 
 python /opt/base/www/manage.py syncdb --noinput
-python /opt/base/www/manage.py collectstatic --noinput
+# python /opt/base/www/manage.py collectstatic --noinput
 
 chown -R www-data: /opt/base/db
 # Enable apache
 cp -r /vagrant/deploy/config/etc /
-a2dissite 000-default
-a2ensite base
+# a2dissite 000-default
+# a2ensite base
